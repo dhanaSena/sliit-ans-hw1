c25c087e5d17cb700610726f5abbbe4d22f1045e
dhanaSena <78255490+dhanaSena@users.noreply.github.com>
2021-01-30 15:05:26 +0530

    Create client_2.py

---
+++ task1/client_2.py
@@ -0,0 +1,26 @@
+#----- A simple TCP client program in Python using send() function -----
+
+import socket
+
+
+
+# Create a client socket
+
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+
+
+
+# Connect to the server
+
+clientSocket.connect(("127.0.0.1",9090));
+
+
+# Receive data from server
+
+dataFromServer = clientSocket.recv(1024);
+
+
+# Print to the console
+
+# Print to the console
+print('Client 2 has received %s from the Server' % (dataFromServer.decode()))
